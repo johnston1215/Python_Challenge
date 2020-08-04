376bf9f0c8e3127b6de3c323f3b0884eaace8062
Johnston1215 <johnston1215@yahoo.com>
2020-08-04 15:56:10 -0500

    Updated for Ben's review

--- pybank/main.py
+++ pybank/main.py
@@ -5,49 +5,88 @@ import csv
 #Create path to raw file
 filePath = os.path.join("resources/" + "02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
 
-#Read raw CSV file
+#Read raw CSV file (with)
 with open(filePath) as csvFile:
     csvReader = csv.reader(csvFile)
-    for row in csvReader:
-        print(row)
+    #for row in csvReader:
+    #       print(row)
 
-#Count number of months included in data
-    months = 0
+    #Count number of months included in data
+    #Ditch (next) header row
+    TotalMonths = 0
+    Total = 0.00
+    MonthlyChange = []
+    PrevMonth = 0.00
+    next(csvReader, None)
     for row in csvReader:
-        months == months + 1
-    print(f"Financial results for the last {months} months of data")
+        TotalMonths = TotalMonths + 1
+    #Get Current profit row[1]
+        Total = Total + int(row[1])
+    #Calculate the net + / - over the presented timeframe
+        PrevMonth = int(row[1]) - PrevMonth
+        MonthlyChange.append(PrevMonth)
+    #Calculate the average change over periods
+        AvgChange = sum(MonthlyChange) / len(MonthlyChange)
 
-#Calculate the net + / - over the presented timeframe
-    total = 0
-    for row in csvReader:
-        total == total + row[1]
-    print(f"Net Income/Loss over the presented timeframe is ${total}")
 
-#Calculate the average change over periods
-#Create list variable to house changes
-    changes = []
-    results = row[1]
-#find changes for every timeframe and add to changes list
-    for row in csvReader:
-        results == row[1]-results
-        changes.append(results)
+    #BEN'S CODE----------------------------
+    #month_of_change = []
+    #net_change_list = []
+    #net_change = int(row[1]) - prev_net
+        #prev_net = int(row[1])
+        #net_change_list += [net_change]
+        #month_of_change += [row[0]]
+    #net_monthly_avg = sum(net_change_list) / len(net_change_list)
 
-#calculate average based on full changes list
-#   average = sum(changes) / len(changes)
-    print(f"The average change in income/loss over each period is ${average}")
+
+    #Find greatest period increase in profits over period
+    GreatestChange = 0
+    for change in MonthlyChange:
+        if change > GreatestChange:
+            change = GreatestChange
+    #Not sure how to reference the month
     
-#Find greatest period increase in profits over period
-    diff = 0
-    for row in csvReader:
-        monchange == (row[1]-(row-1[1])
-        if (monchange>diff):
-            diff == monchange
-            monchange == 0
-
-#Find greatest decrease in loss over period
+    #Find greatest decrease in loss over period
+    GreatestDecrease = 0
+    for change in MonthlyChange:
+        if change < GreatestDecrease:
+            change = GreatestDecrease
+    #Not sure how to reference the month
 
 
-#Print results in terminal window
+    #Print results in terminal window
+    print("Financial Analysis")
+    print("------------------")
+    print(f"Total months " + str(TotalMonths))
+    #print (TotalMonths)
+    #Print Total
+    print (f"Total $" + str(Total))
+    #Print Average Change
+    print(f"Average Change $" + str(AvgChange))
+    #print (AvgChange)
+
+#Write data to text file
 
 
-#Export text file with results
+#FOR PYPOLL-COPY LATER
+#Open File
+
+#Create variables - totalVotes int, candidateList = {candidate: votes}
+
+#Open CSV for read
+
+#for each row
+#total votes = totalVotes + 1
+#if candidateList.index(current row candidate) < 1:
+    #append dict to list and set votes = 0
+
+#increase vote for candidate + 1
+
+#exit for loop
+
+#create temp variables
+#For candidate in the list
+#   find one with most votes ( accumulator? )
+#   calc and print percentage
+
+#print output
