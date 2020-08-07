8d8c06dbd02d44d378d2391f970c1a961126792b
Johnston1215 <johnston1215@yahoo.com>
2020-08-07 17:36:44 -0500

    Updated 8_7 PM

--- pypoll/main.py
+++ pypoll/main.py
@@ -19,18 +19,25 @@ with open(filePath) as csvFile:
     for row in csvReader:
         totalVotes = totalVotes + 1
 
-    if candidateList.index(row[2]) < 0:
-        candidateList.append(row[2])
-        candidatevotelist.append(1)
-            else:
-                candidatevotelist[candidateList.index(row[2])] += 1
+        #Identify a new candidate
+        #if candidateList.index(row[2]) < 0:
+        #    candidateList.append(row[2])
+        #    candidatevotelist.append(1)
+            #else:
+            #    candidatevotelist[candidateList.index(row[2])] += 1
     #create temp variables
     
     #For candidate in the list
 
-
     #   find one with most votes ( accumulator? )
     #   calc and print percentage
+
+    #Write data to text file
+    with open("Analysis/" + "PyPollAnalysis.txt", "w") as out_file:
+        out_file.write("Election Results\n")
+        out_file.write("----------------\n")
+        out_file.write(f"Total Votes: " + str(totalVotes))
+        out_file.write("\n----------------")
 
     #print output
     print("Election Results")
